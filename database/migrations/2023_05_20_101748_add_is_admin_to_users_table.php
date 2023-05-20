7c039c26511577ee6e44e5d62cae29ccf85e57f7
lypsisrudiansyah <rudiansyahether@gmail.com>
2023-05-20 17:29:31 +0700

    feat: create IsAdminMiddleware.php and 2023_05_20_101748_add_is_admin_to_users_table.php

---
+++ database/migrations/2023_05_20_101748_add_is_admin_to_users_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->boolean('is_admin')->after('remember_token')->default(false);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            if (Schema::hasColumn('users', 'is_admin'))
+            {
+                $table->dropColumn('is_admin');
+            }
+        });
+    }
+};
