b67ec3c153a0872fe6043b6cc2320c967f2d80e5
lypsisrudiansyah <rudiansyahether@gmail.com>
2023-05-20 14:10:20 +0700

    🔒 chore(web.php): add auth middleware to products resource route
    🔒 chore(AuthTest.php): add test_login_redirect_to_products and test_user_unauthenticated_cannot_access_product tests and create user in setUp method

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('products', ProductController::class);
+Route::resource('products', ProductController::class)->middleware('auth');
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
--- tests/Feature/AuthTest.php
+++ tests/Feature/AuthTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -13,10 +14,39 @@ class AuthTest extends TestCase
      *
      * @return void
      */
-    public function test_example()
-    {
-        $response = $this->get('/');
+    
+    protected $user;
+    protected $password = '1234';
 
-        $response->assertStatus(200);
+    public function setUp(): void
+    {
+        parent::setUp();
+
+        $this->user = User::create([
+            'name' => 'rudi',
+            'email' => 'rudio@mail.com',
+            'password' => bcrypt($this->password)
+        ]);
+        // $this->user = User::whereName('rudi')->first();
+    }
+    
+    public function test_login_redirect_to_products()
+    {
+        $this->post('login', [
+            'email' => $this->user->email,
+            'password' => $this->password,
+        ]);
+
+
+    }
+
+    public function test_user_unauthenticated_cannot_access_product()
+    {
+        $response = $this->get('/products');
+        
+        // This is the expected behavior of Laravel validation if the validation does not pass
+        $response->assertStatus(302);
+        $response->assertRedirect('login');
+
     }
 }
