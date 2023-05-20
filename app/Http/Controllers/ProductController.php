52cd68d7e93b1d38849c54d3a3012c592f70fc73
lypsisrudiansyah <rudiansyahether@gmail.com>
2023-05-20 17:29:39 +0700

    update ProductController.php and Kernel.php

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -69,7 +69,7 @@ class ProductController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, int $id)
     {
         //
     }
